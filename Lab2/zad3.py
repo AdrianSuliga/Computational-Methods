2f77647d9b02a2b53475df50d15f063f6dff7530
Adrian Suliga <adriansuliga81@gmail.com>
2025-03-18 14:53:41 +0100

    Add cycle code notes

--- Lab2/zad3.py
+++ Lab2/zad3.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 
 def readGraph(path: str):
     file = open(path, "r")
-    G = nx.Graph()
+    E = []
 
     for line in file:
         u, v, w = map(int, line.split(','))
-        G.add_edge(u, v, weight = w) 
+        E.append((u, v, w))
+     
+    return E
 
-    return G
-
-def drawGraph(G):
+def drawGraph(E):
+    # Does not work for now
+    G = nx.from_edgelist(E)
     pos = nx.spring_layout(G, seed = 3)
 
     nx.draw_networkx_nodes(G, pos, node_size = 400)
@@ -24,4 +26,54 @@ def drawGraph(G):
     plt.title("Przykładowy graf")
     plt.show()
 
-G = readGraph("input/graph.txt")
+def visited(node, path):
+    return node in path
+
+# Function to rotate a cycle such that 
+# it starts with the smallest node
+def rotate_to_smallest(path):
+    min_index = path.index(min(path))
+    return path[min_index:] + path[:min_index]
+
+# Function to invert the cycle order
+def invert(path):
+    return rotate_to_smallest(path[::-1])
+
+# Function to check if a cycle is new
+def is_new(path, cycles):
+    return path not in cycles
+
+def findOneNewCycle(path, graph, cycles):
+    start_node = path[0]
+    
+    # Visit each edge and node of each edge
+    for node1, node2, _ in graph:
+        if start_node in (node1, node2):
+            next_node = node2 if node1 == start_node else node1
+            
+            if not visited(next_node, path):
+                # Neighbor node not on path yet
+                sub = [next_node] + path
+                # Explore extended path
+                findOneNewCycle(sub, graph, cycles)
+            elif len(path) > 2 and next_node == path[-1]:
+                # Cycle found
+                p = rotate_to_smallest(path)
+                inv = invert(p)
+
+                if is_new(p, cycles) and is_new(inv, cycles):
+                    cycles.append(p)
+
+def findAllCycles(E):
+    cycles = []
+
+    for u, v, _ in E:
+        findOneNewCycle([u], E, cycles)
+        findOneNewCycle([v], E, cycles)
+
+    return cycles
+
+E = readGraph("input/graph.txt")
+print(E)
+print(findAllCycles(E))
+
