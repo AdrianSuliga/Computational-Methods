aa53d0298b27a3ec3fe8b4c8ee6871d4e8c8e323
Adrian Suliga <adriansuliga81@gmail.com>
2025-03-20 23:33:31 +0100

    Solve ex 3 using kirchhoff laws

--- Lab2/zad3.py
+++ Lab2/zad3.py
@@ -1,3 +1,4 @@
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -11,69 +12,140 @@ def readGraph(path: str):
      
     return E
 
-def drawGraph(E):
-    # Does not work for now
-    G = nx.from_edgelist(E)
-    pos = nx.spring_layout(G, seed = 3)
+def list_form(E):
+    n = graph_size_from_list(E)
+    G = [[] for _ in range(n)]
 
-    nx.draw_networkx_nodes(G, pos, node_size = 400)
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=3)
+    for edge in E:
+        G[edge[0]].append((edge[1], edge[2]))
+        G[edge[1]].append((edge[0], edge[2]))
 
-    nx.draw_networkx_labels(G, pos, font_size=16, font_family="sans-serif")
-    edge_labels = nx.get_edge_attributes(G, "weight")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels)
+    return G
 
-    plt.title("Przykładowy graf")
-    plt.show()
-
-def visited(node, path):
-    return node in path
-
-# Function to rotate a cycle such that 
-# it starts with the smallest node
-def rotate_to_smallest(path):
-    min_index = path.index(min(path))
-    return path[min_index:] + path[:min_index]
-
-# Function to invert the cycle order
-def invert(path):
-    return rotate_to_smallest(path[::-1])
-
-# Function to check if a cycle is new
-def is_new(path, cycles):
-    return path not in cycles
-
-def findOneNewCycle(path, graph, cycles):
-    start_node = path[0]
-    
-    # Visit each edge and node of each edge
-    for node1, node2, _ in graph:
-        if start_node in (node1, node2):
-            next_node = node2 if node1 == start_node else node1
-            
-            if not visited(next_node, path):
-                # Neighbor node not on path yet
-                sub = [next_node] + path
-                # Explore extended path
-                findOneNewCycle(sub, graph, cycles)
-            elif len(path) > 2 and next_node == path[-1]:
-                # Cycle found
-                p = rotate_to_smallest(path)
-                inv = invert(p)
-
-                if is_new(p, cycles) and is_new(inv, cycles):
-                    cycles.append(p)
-
-def findAllCycles(E):
-    cycles = []
+def graph_size_from_list(E):
+    size = 0
 
     for u, v, _ in E:
-        findOneNewCycle([u], E, cycles)
-        findOneNewCycle([v], E, cycles)
+        size = max(size, u, v)
+    
+    return size + 1
 
+def drawGraph(Edges, Weights, Currents):
+    G = nx.DiGraph()
+
+    for u, v, _ in Edges:
+        w = Weights[Currents[u][v]]
+        if u < v and w < 0: G.add_edge(v, u, weight = abs(w))
+        elif u > v and w < 0: G.add_edge(u, v, weight = abs(w))
+        elif u < v and w >= 0: G.add_edge(u, v, weight = w)
+        elif u > v and w >= 0: G.add_edge(v, u, weight = w)
+        
+    pos = nx.spring_layout(G, seed = 89)
+    nx.draw_networkx_nodes(G, pos, node_color="lightblue", edgecolors="black")
+    nx.draw_networkx_edges(G, pos, arrowstyle="->", arrowsize=15)
+    nx.draw_networkx_labels(G, pos, font_size=12, font_color="black")
+
+    edge_labels = {(u, v): f"{w:.2f}" for u, v, w in G.edges(data="weight")}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.tight_layout()
+    plt.show()
+
+def findCycleFrom(G:list, u:int, path:list, cycles:list, size:int):
+    for v, _ in G[u]:
+        if v not in path:
+            findCycleFrom(G, v, [v] + path, cycles, size)
+        elif v == path[-1] and len(path) > 2:
+            min_index = path.index(min(path))
+            path = path[min_index:] + path[:min_index]
+            inv = [path[0]] + path[1:][::-1]
+            if path not in cycles and inv not in cycles and len(cycles) < size:
+                cycles.append(path)
+
+def findCycles(G, size):
+    cycles = []
+    n = len(G)
+
+    for u in range(n):
+        findCycleFrom(G, u, [u], cycles, size)
+        if len(cycles) == size: break
+
+    if len(cycles) < size:
+        print("Nie można znaleźć wystarczająco dużo cykli!")
+    
     return cycles
 
-E = readGraph("input/graph.txt")
-print(E)
-print(findAllCycles(E))
+def findPath(G, start, end):
+    def dfsVisit(G, u, end, visited, path):
+        visited[u] = True
+        for v, _ in G[u]:
+            if v == end:
+                return path + [v]
+            if not visited[v]:
+                return dfsVisit(G, v, end, visited, path + [v])
+
+    n = len(G)
+    visited = [False for _ in range(n)]
+    visited[start] = True
+    path = [start]
+
+    return dfsVisit(G, start, end, visited, path)
+
+def edgesToCurrents(V, Edges):
+    Currents = [[-1 for _ in range(V)] for _ in range(V)]
+    it = 0
+    
+    for u, v, _ in Edges:
+        Currents[u][v] = it
+        Currents[v][u] = it
+        it += 1
+
+    return Currents
+
+def zad3Kirchoff(Edges, s, t, SEM):
+    G = list_form(Edges)
+    E = len(Edges)
+    V = len(G)
+
+    Eqs = [[0 for _ in range(E)] for _ in range(E)]
+    Currents = edgesToCurrents(V, Edges)
+
+    # Wektor wyników
+    A = [0 for _ in range(E)]
+    A[0] = SEM
+
+    eq_row = 0
+
+    # Ścieżka z SEM
+    SEM_path = findPath(G, s, t)
+    for i in range(len(SEM_path) - 1):
+        Eqs[eq_row][Currents[SEM_path[i]][SEM_path[i + 1]]] = Edges[Currents[SEM_path[i]][SEM_path[i + 1]]][2] * (1 if SEM_path[i] > SEM_path[i + 1] else -1)
+    eq_row += 1
+
+    # 1. prawo Kirchoffa
+    for u in range(V):
+        if u == s or u == t: continue
+        for v, _ in G[u]:
+            if u < v: Eqs[eq_row][Currents[u][v]] = -1
+            if u > v: Eqs[eq_row][Currents[u][v]] = 1
+        eq_row += 1
+
+    # 2. prawo Kirchoffa
+    Cycles = findCycles(G, E - V + 1)
+
+    for cycle in Cycles:
+        cycle = cycle + [cycle[0]]
+        cycle_len = len(cycle)
+
+        for i in range(cycle_len - 1):
+            Eqs[eq_row][Currents[cycle[i]][cycle[i + 1]]] = Edges[Currents[cycle[i]][cycle[i + 1]]][2] * (1 if cycle[i] > cycle[i + 1] else -1)
+        eq_row += 1
+
+    Weights = np.linalg.solve(Eqs, A)
+    print("Wyliczone natężenia:")
+    print(Weights)
+
+    drawGraph(Edges, Weights, Currents)
+
+zad3Kirchoff(readGraph("input/graph1.txt"), 1, 3, 200)
+zad3Kirchoff(readGraph("input/graph2.txt"), 0, 1, 50)
 
